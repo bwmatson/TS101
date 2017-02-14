30571366a6dc8e7cd90007ffb6fe4fb17953637a
bwmatson <benjamin.matson@live.com>
2017-02-13 19:13:49 -0600

    Decorator

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ hello('TypeScript 2');
 
 /* Mixins Example
     *Allows for classes to be broken down into small pieces and mixed and matched as needed.
-    *Uses the 'implements' key work and can accept an unlimited number of classes 
+    *Uses the 'implements' key work and can accept an unlimited number of classes
     *   note: extends only allows the use of one class
 */
 import { applyMixins } from './mixins/applyMixins';
@@ -38,3 +38,49 @@ class Duck implements Swimmer, Talker, Walker {
 applyMixins(Duck, [Swimmer, Talker, Walker]);
 // Uncomment to Run Example
 // const duck = new Duck();
+
+
+/* Decorator Example
+    * A Decorator is a special kind of declaration that can be attached to a:
+    *    class declaration, method, accessor, property, or parameter.
+    * Decorators use the form @expression, where expression must evaluate to a function
+        that will be called at runtime with information about the decorated declaration.
+    * There are 4 types of Decorators:
+        Class - <TFunction extends Function>(target: TFunction) => TFunction | void;
+        Property - (target: Object, propertyKey: string | symbol) => void;
+        Method -
+            <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>)
+            => TypedPropertyDescriptor<T> | void;
+        Parameter = (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
+    * Each Decorator describes calls a function that pulls in the followinf form what it is decorating
+        target - item being decorated
+        propertyKey - name of item being decorated
+        discriptor - a property descriptor of the given property if it exists on the object, undefined otherwise
+        parameterIndex - index of parameter being initialized
+
+    *Note: Decorators are experimental (may change in future releases), but are actively
+        used in Frameworks like Angular2
+    *tsconfig.json must have this feature enabled
+    {
+        "compilerOptions": {
+            "target": "ES5",
+            "emitDecoratorMetadata": true
+            "experimentalDecorators": true
+        }
+    }
+*/
+
+function logMethodName(target: any, key: string) {
+    console.log('logMethodName: ' + key);
+}
+
+class DecoratorExample {
+    public hello: string;
+    constructor() {
+        this.hello = 'hello';
+    }
+
+    // Uncomment to Run Example
+    // @logMethodName
+    public method() {}
+}
